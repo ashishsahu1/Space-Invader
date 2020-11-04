3eb5a328ae90ffcfb339b78243ad724904f3b2db
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-05 05:08:51 +0530

    closing button working

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ pygame.init()
 #creating screen
 screen = pygame.display.set_mode((800,600))
 
-
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            running = False
