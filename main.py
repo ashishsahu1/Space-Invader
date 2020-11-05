e200f871f63256021671020ba709f07ead9a62f2
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-05 05:43:22 +0530

    added player function

--- main.py
+++ main.py
@@ -11,12 +11,23 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('img/icon2.png')
 pygame.display.set_icon(icon)
 
+#player
+playerImg = pygame.image.load('img/space.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg,(playerX,playerY))
+
 # Game loop
 running = True
 while running:
+    screen.fill((15, 48, 87))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running = False    
 
-    screen.fill((15, 48, 87))
+    player()
+
+    
     pygame.display.update()
