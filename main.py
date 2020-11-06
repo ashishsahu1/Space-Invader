cfcba847cdebc3bb4fc42dd79e70f7de36bac7e4
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-06 12:59:29 +0530

    background image

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ pygame.init()
 #creating screen (x,y)
 screen = pygame.display.set_mode((800,600))
 
+background = pygame.image.load('img/bg.png')
+
 #title and icon
 pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('img/icon2.png')
@@ -17,7 +19,7 @@ pygame.display.set_icon(icon)
 enemyImg = pygame.image.load('img/enemy.png')
 enemyX = random.randint(0,800)
 enemyY = random.randint(50,150)
-enemyXchange = 0.2
+enemyXchange = 4
 enemyYchange = 40
 def enemy(enemyX,enemyY):
     screen.blit(enemyImg,(enemyX,enemyY))
@@ -38,6 +40,9 @@ while running:
     #screen colour
     screen.fill((15, 48, 87))
 
+    #background
+    screen.blit(background,(0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False  
@@ -45,9 +50,9 @@ while running:
     #checking keystroke is left or right
         if event.type  == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerXchange = -0.3
+                playerXchange = -5
             if event.key == pygame.K_RIGHT:
-                playerXchange = 0.3
+                playerXchange = 5
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -63,10 +68,10 @@ while running:
     #enenmy movement
     enemyX += enemyXchange
     if enemyX  >= 736:
-        enemyXchange = -0.2
+        enemyXchange = -4
         enemyY += enemyYchange
     elif enemyX  <=0:
-        enemyXchange  = 0.2
+        enemyXchange  = 4
         enemyY += enemyYchange
 
     
